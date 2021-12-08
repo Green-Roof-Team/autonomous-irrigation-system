8be1c54f44464e206a646e0af954bb5bfdd5dbbd
ranamerp <ranamerp@gmail.com>
2021-12-08 19:24:22 +0000

    Added data

--- WindGetData.py
+++ WindGetData.py
@@ -1,10 +1,19 @@
 import serial
 import re
 #import time
-arduino_port = "/dev/ttyACM1" #serial port of Arduino
-baud = 9600 #arduino uno runs at 9600 baud
+ports = ["/dev/ttyACM0", "/dev/ttyACM1", "/dev/ttyACM2", "/dev/ttyACM3"]
+baud = 9600
+for port in ports:
+    try:
+        arduino_port = port
+        ser = serial.Serial(port, baud)
+        break
+    except serial.serialutil.SerialException as e:
+        pass
+#arduino_port = "/dev/ttyACM0" #serial port of Arduino
+#baud = 9600 #arduino uno runs at 9600 baud
 fileName="analog-data.csv" #name of the CSV file generated
-ser = serial.Serial(arduino_port, baud)
+#ser = serial.Serial(arduino_port, baud)
 print("Connected to Arduino port:" + arduino_port)
 file = open(fileName, "a") #appends
 print("Created file")
